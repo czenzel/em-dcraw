819bc9f79a7dae02015863402972b8442e703c16
Christopher David Zenzel <czenzel@zenzel.net>
2017-09-08 05:49:30 -0400

    Add ImageMagick Conversion
    Add ImageMagick Conversion Tool

--- build.sh
+++ build.sh
@@ -15,6 +15,12 @@ emmake make
 emmake make install
 cd ..
 
+cd ImageMagick
+emconfigure ./configure --prefix=`pwd`/../build
+emmake make
+emmake make install
+cd ..
+
 cd build/lib
 emcc -o libjpeg.js libjpeg.dylib
 
@@ -24,8 +30,13 @@ emcc -o dcraw ../../src/dcraw.c -DNO_JASPER -DNO_LCMS -I../include -L../lib -ljp
 cp dcraw dcraw.bc
 emcc -o dcraw.js -s ALLOW_MEMORY_GROWTH=1 -O3 dcraw.bc
 
+cp convert convert.bc
+emcc convert.bc ../lib/libMagickWand-7.Q16HDRI.a ../lib/libMagickCore-7.Q16HDRI.a -O3 -s ALLOW_MEMORY_GROWTH=1 -o convert.js
+
 cd ../..
 
+cp build/bin/convert.js js/
+cp build/bin/convert.js.mem js/
 cp build/bin/dcraw.js js/
 cp build/lib/libjpeg.js js/
 cp build/bin/dcraw.js.mem js/
